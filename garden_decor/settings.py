fbc3499ad3d574a26db49ebb36e5e0fe95985c02
Artem822 <artem822.kovalenko@gmail.com>
2026-03-24 20:13:03 +0300

    db change

--- garden_decor/settings.py
+++ garden_decor/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'garden_decor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'sad.sqlite3',
     }
 }
 
